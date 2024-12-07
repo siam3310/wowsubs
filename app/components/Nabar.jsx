cd257aa0347e9d098c575bf57f0aea93b2c4b262
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-12-07 14:28:28 +0600

    Update Nabar.jsx

--- app/components/Nabar.jsx
+++ app/components/Nabar.jsx
@@ -8,16 +8,16 @@ const Navbar = () => {
       <div className="navbar px-4 sm:px-[2rem] xl:px-[8rem] bg-base-100">
         <div className="flex-1">
           <div className="btn btn-ghost normal-case  text-xl">
-            <Image src={logo} className="h-full w-full" alt="wowsubs" />
+            <Image src={logo} className="h-full w-full" alt="bpl25" />
           </div>
         </div>
         <div className="flex-1  sm:flex hidden justify-center italic text-xl text-[2rem] font-semibold">
-          Watch Live Cricket Worldcup
+          Watch Live BPL 
         </div>
         <div className="flex-1 flex justify-end">
           <Link
             className="btn btn-ghost normal-case  text-xl "
-            href={"https://umer-rauf-portfolio.netlify.app/"}
+            href={"/"}
           >
             Home Page
           </Link>

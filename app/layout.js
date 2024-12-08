60706974388d602bb2624d153bd507012cc7ca8e
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-12-08 13:18:49 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "BPL 2025 LIVE - bpl25.netlify.app",
   description: "A website where you can watch live BPL 2025 for free - BPL25.NETLIFY.APP",
+  verification: { google: "keVTh4CE7NNuJ2uokcjoiAlHfUlakVP7ThDhRKijtCc" },
 };
 
 const containerStyle = {

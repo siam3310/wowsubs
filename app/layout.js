fb597559e3f52837be6d67282f78b16bcc12bdb0
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2024-12-09 12:09:14 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -22,25 +22,24 @@ const containerStyle = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        {/* Google Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C00L020W1Z"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-C00L020W1Z');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <div className="h-screen" style={containerStyle}>
           <Navbar />
           {children}
-          {/* Visitor Counter */}
-          <div className="flex justify-center mt-4">
-            <a
-              href="https://www.freecounterstat.com"
-              title="web page counters free"
-            >
-              <img
-                src="https://counter6.optistats.ovh/private/freecounterstat.php?c=sy78cebaseknw8yde4e6jr5b6x3etltl"
-                border="0"
-                title="web page counters free"
-                alt="web page counters free"
-              />
-            </a>
-          </div>
           <Analytics />
         </div>
       </body>

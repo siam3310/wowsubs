bdf507d92c114cd02c1ee1d6d7f26f249c0e974e
Mahamudun nabi siam <mahamudunn1005@gmail.com>
2024-12-10 12:31:59 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,44 +4,32 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <main
-        className="backdrop-blur-sm mx-[2rem] min-[450px]:mx-[8rem] sm:mx-[2rem] xl:mx-[20rem]"
-        style={{ borderRadius: "15px" }}
-      >
-        <div className="flex flex-col sm:flex-row rounded-lg mt-8">
-          <div className="sm:flex-1">
-            <Image
-              src={poster}
-              className="h-full object-cover"
-              alt="Cricket Poster"
-              style={{ borderRadius: "5%" }}
-            />
-          </div>
-          <div className="p-4 sm:flex-1 text-white">
-            <h2 className="text-xl font-bold">Watch Live BPL 2025</h2>
-            <p className="mt-1 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-semibold">
-              Without Any Cost
-            </p>
-            <Link
-              href="/watchlive"
-              className="w-full mt-4 inline-block py-3 px-6 text-center text-lg font-bold rounded-lg bg-gradient-to-r from-indigo-500 to-purple-600 text-white transition-all duration-300 hover:scale-105 hover:shadow-lg"
-            >
-              Watch Now
-            </Link>
-          </div>
+    <main
+      className="backdrop-blur-sm mx-[2rem] min-[450px]:mx-[8rem] sm:mx-[2rem] xl:mx-[20rem]"
+      style={{ borderRadius: "15px" }}
+    >
+      <div className="flex flex-col sm:flex-row rounded-lg mt-8">
+        <div className="sm:flex-1">
+          <Image
+            src={poster}
+            className="h-full object-cover"
+            alt="Cricket Poster"
+            style={{ borderRadius: "5%" }}
+          />
         </div>
-      </main>
-
-      {/* Widget Section */}
-      <div className="mt-6 mx-[2rem] min-[450px]:mx-[8rem] sm:mx-[2rem] xl:mx-[20rem]">
-        <iframe
-          src="https://cwidget.crictimes.org/?v=1.1&a=000000&c=ffffff&lc=ff0000"
-          style={{ width: "100%", minHeight: "460px", border: "none" }}
-          frameBorder="0"
-          scrolling="yes"
-        ></iframe>
+        <div className="p-4 sm:flex-1 text-white">
+          <h2 className="text-xl font-bold">Watch Live BPL 2025</h2>
+          <p className="mt-1 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-semibold">
+            Without Any Cost
+          </p>
+          <Link
+            href="/watchlive"
+            className="w-full mt-4 inline-block py-3 px-6 text-center text-lg font-bold rounded-lg bg-gradient-to-r from-indigo-500 to-purple-600 text-white transition-all duration-300 hover:scale-105 hover:shadow-lg"
+          >
+            Watch Now
+          </Link>
+        </div>
       </div>
-    </>
+    </main>
   );
 }
